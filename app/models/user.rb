91d4d7caa4e06c5c2b63af2d40b76830e76fb9fa
daniel <daniel@daniels-MacBook-Pro-2.local>
2017-02-17 13:37:12 -0800

    Create users table and model

---
+++ app/models/user.rb
@@ -0,0 +1,44 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :user_name, :password_digest, presence: true
+  validates :user_name, :session_token, uniqueness: true
+  validates :password, length: { minimum: 4 }
+  validate :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+end
